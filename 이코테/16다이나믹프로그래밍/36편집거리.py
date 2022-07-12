7447995fb5fd06d2b1c8fa20222f2c3fd7640aad
YDbata <kiz75@naver.com>
2022-07-13 01:06:15 +0900

    [이코테] 36번 풀이 완성
    36편집거리 풀이 완성

--- 이코테/16다이나믹프로그래밍/36편집거리.py
+++ 이코테/16다이나믹프로그래밍/36편집거리.py
@@ -11,14 +11,21 @@ for aa in range(alen):
 re = 0
 tmp = 0
 idx = 0
-for i in range(alen - 1):
+for i in range(alen):
     if al[i] == -1:
         tmp += 1
     elif idx < al[i]:
         if tmp == 0:
             re += al[i] - idx
         else:
+            re += tmp
             tmp = 0
-        idx = al[i]
+        idx = al[i] + 1
+    else:
+        idx += 1
 
-    idx += 1
+    # print(re, tmp, idx, al[i])
+
+print(re + tmp)
+'''sunxday
+saturday'''
