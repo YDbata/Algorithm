46f8aaa970c4a39c5bd2535f1fcc71be5c5933c2
YDbata <kiz75@naver.com>
2022-07-14 01:36:25 +0900

    [이코테] 37완성 38시작
    37플로이드완성
    38정확한순위 시작

--- 이코테/17최단거리/37플로이드.py
+++ 이코테/17최단거리/37플로이드.py
@@ -1,10 +1,26 @@
+import sys
+
 n = int(input())
 m = int(input())
-
-board = [[0 for _ in range(n)] for _ in range(n)]
+inf = 100001*n
+board = [[inf for _ in range(n)] for _ in range(n)]
 
 for i in range(m):
-    tmp = list(map(int, input().split()))
-    board[tmp[0] - 1][tmp[1] - 1] = tmp[2]
+    tmp = list(map(int, sys.stdin.readline().split()))
+    board[tmp[0] - 1][tmp[1] - 1] = min(board[tmp[0] - 1][tmp[1] - 1], tmp[2])
 
-print(board)
+for i in range(n):
+    board[i][i] = 0
+
+for k in range(n):
+    for x in range(n):
+        for y in range(n):
+            board[x][y] = min(board[x][y], board[x][k] + board[k][y])
+
+for x in range(n):
+    for y in range(n):
+        if board[x][y] == inf:
+            board[x][y] = 0
+
+for i in range(n):
+    print(*board[i], sep=" ")
