e0689255a8cc746629e8a7b8736cbbdf5006a7b6
YDbata <kiz75@naver.com>
2022-07-17 23:30:04 +0900

    [이코테] 38번

--- 이코테/17최단거리/38정확한순위.py
+++ 이코테/17최단거리/38정확한순위.py
@@ -1,9 +1,20 @@
 n, m = map(int, input().split())
 
-board = [[0 for _ in range(n)] for _ in range(n)]
+inf = 10001*n
+board = [[inf for _ in range(n)] for _ in range(n)]
 
 for _ in range(m):
     a, b = map(int, input().split())
     board[a][b] = b
 
+# 플로이설
+for k in range(n):
+    for a in range(n):
+        for b in range(n):
+            board[a][b] = min(board[a][b], board[a][k] + board[k][b])
+
 print(board)
+re = 0
+for b in board:
+    if b.count(inf) == 1:
+        pass
