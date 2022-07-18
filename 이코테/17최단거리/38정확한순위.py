f7ea180de06664126e405c56c3437526de75eed3
YDbata <kiz75@naver.com>
2022-07-18 23:47:34 +0900

    38번 완성

--- 이코테/17최단거리/38정확한순위.py
+++ 이코테/17최단거리/38정확한순위.py
@@ -5,7 +5,7 @@ board = [[inf for _ in range(n)] for _ in range(n)]
 
 for _ in range(m):
     a, b = map(int, input().split())
-    board[a][b] = b
+    board[a - 1][b - 1] = b
 
 # 플로이설
 for k in range(n):
@@ -13,8 +13,23 @@ for k in range(n):
         for b in range(n):
             board[a][b] = min(board[a][b], board[a][k] + board[k][b])
 
-print(board)
+# print(board)
 re = 0
-for b in board:
-    if b.count(inf) == 1:
-        pass
+for i in range(n):
+    tmp = n - board[i].count(inf)
+    for j in range(n):
+        if board[j][i] != inf:
+            tmp += 1
+
+    if tmp >= n - 1:
+        re += 1
+
+print(re)
+
+'''
+알게 된것
+초기화 가능
+숫자 카운트시 if문
+if board[i][j] != inf or board[j][i] != inf:
+    tmp += 1
+'''
