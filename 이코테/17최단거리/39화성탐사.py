bd6172ba27344840f27d8c745ca6cf0b8de70cff
YDbata <kiz75@naver.com>
2022-07-20 22:47:34 +0900

    [이코테] 야근

--- 이코테/17최단거리/39화성탐사.py
+++ 이코테/17최단거리/39화성탐사.py
@@ -6,3 +6,6 @@ for _ in range(t):
     for _ in range(n):
         board.append(list(map(int, input().split())))
 
+    memo = [[-1 for _ in range(n)] for _ in range(n)]
+    memo[0][0] = board[0][0]
+
